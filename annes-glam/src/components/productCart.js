370cb53a00d3e631afa40cf3294001db4d979d94
Josinimasango <nellyjossydumako@gmail.com>
2024-08-27 16:46:11 +0200

    adding product cart

--- annes-glam/src/components/productCart.js
+++ annes-glam/src/components/productCart.js
@@ -19,7 +19,7 @@ const ProductCart=(props) => {
   return (
     <div className='bg-gray-200 p-5 rounded-xl shadow-sm'>
        <Link to={slug}>
-          <img src={image} alt='' className='w-full h-88 object-cover object-top object-top drop-shadow-[0_80px_30px_#0007]' />
+          <img src={image} alt='' className='w-full h-87 object-cover object-top object-top drop-shadow-[0_80px_30px_#0007]' />
        </Link>
        <h3 className='text-2xl py-3 text-center font-medium'>{name}</h3>
        <div className='flex justify-between items-center'>
