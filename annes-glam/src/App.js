86420e1b6e0cb28ffa1ecffc1da4df8a5de881fd
Josinimasango <nellyjossydumako@gmail.com>
2024-08-27 17:08:41 +0200

    adding routing

--- annes-glam/src/App.js
+++ annes-glam/src/App.js
@@ -4,11 +4,10 @@ import Home from './pages/home';
 import Detail from './pages/detail';
 import PaymentForm from './components/PaymentForm'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Elements } from '@stripe/react-stripe-js'; // Import Elements from @stripe/react-stripe-js
-import { loadStripe } from '@stripe/stripe-js'; // Import loadStripe from @stripe/stripe-js
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js'; 
 import CompleteOrder from './components/CompleteOrder';
 
-// Replace with your actual Stripe public key
 const stripePromise = loadStripe('pk_test_51Pqf9jP6pFU5eeDnmOtsAIwHBUmgx68GIqm1gt3EvrlcJu7XHoVULwuVPGCsEX79iCU2Kr2fh1HJZQky63F1I9Rm00J1nfbG7V');
 
 function App() {
